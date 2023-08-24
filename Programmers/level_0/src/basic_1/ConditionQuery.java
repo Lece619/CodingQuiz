6d90bd3c674473e5c3fa95788aff185fa470186a
Lece <lece619@gmail.com>
2023-08-24 22:53:25 +0900

    Solve(Level 0): 코딩 기초 트레이닝 조건 문자열
    BiFunction 사용

--- Programmers/level_0/src/basic_1/ConditionQuery.java
+++ Programmers/level_0/src/basic_1/ConditionQuery.java
@@ -1,5 +1,8 @@
 package basic_1;
 
+import java.util.Map;
+import java.util.function.BiFunction;
+
 public class ConditionQuery {
     public int solution(String ineq, String eq, int n, int m) {
         if(eq.equals("=")){
@@ -12,5 +15,15 @@ public class ConditionQuery {
             return n > m ? 1 : 0;
         }
         return n < m ? 1 : 0;
+    }
+
+    public int solutionBiFunction(String ineq, String eq, int n, int m) {
+        Map<String, BiFunction<Integer, Integer, Integer>> functions = Map.of(
+                ">!", (a, b) -> a > b ? 1 : 0,
+                "<!", (a, b) -> a < b ? 1 : 0,
+                ">=", (a, b) -> a >= b ? 1 : 0,
+                "<=", (a, b) -> a <= b ? 1 : 0
+        );
+        return functions.get(ineq + eq).apply(n, m);
     }
 }
